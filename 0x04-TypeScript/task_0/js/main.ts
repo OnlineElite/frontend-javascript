a89356b46a9b9a0baa66afdbe3e6cb32347b837e
OnlineElite <online.0.elite@gmail.com>
2025-07-10 13:32:44 +0100

    adding task 0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,42 @@
+interface Student {
+    firstName : string
+    lastName : string
+    age : number
+    location  : string
+
+}
+
+const student1 : Student = {
+    firstName : "Jamal",
+    lastName : "Boujbari",
+    age : 28,
+    location : "Casablanca"
+}
+
+const student2 : Student = {
+        firstName : "Reda",
+        lastName : "Allam",
+        age : 31,
+        location : "Bouskoura"
+    }
+
+const studentsList : Student[]= [student1, student2]
+
+const table = document.createElement("table")
+
+studentsList.forEach((student) => {
+    const row = document.createElement("tr")
+
+    const nameCell = document.createElement ("td")
+    nameCell.textContent = student.firstName;
+
+    const locationCell = document.createElement("td")
+    locationCell.textContent = student.location;
+
+    row.appendChild(nameCell)
+    row.appendChild(locationCell)
+
+    table.appendChild(row)
+})
+
+document.body.appendChild(table)
